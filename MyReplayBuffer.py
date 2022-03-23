014a313b709bd2200cc08f787bb2736f51487315
Amaranth819 <33890170+Amaranth819@users.noreply.github.com>
2022-03-23 01:11:41 -0400

    Add files via upload

--- MyReplayBuffer.py
+++ MyReplayBuffer.py
@@ -1,1 +1,85 @@
+import warnings
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Generator, List, Optional, Union
+
+import numpy as np
+import torch as th
+import random
+from gym import spaces
+
+from stable_baselines3.common.preprocessing import get_action_dim, get_obs_shape
+from stable_baselines3.common.type_aliases import (
+    DictReplayBufferSamples,
+    DictRolloutBufferSamples,
+    ReplayBufferSamples,
+    RolloutBufferSamples,
+)
+from stable_baselines3.common.vec_env import VecNormalize
 from stable_baselines3.common.buffers import ReplayBuffer
+
+
+class MyReplayBuffer(ReplayBuffer):
+    def __init__(self, 
+            buffer_size: int, 
+            observation_space: spaces.Space, 
+            action_space: spaces.Space, 
+            device: Union[th.device, str] = "cpu", 
+            n_envs: int = 1, 
+            optimize_memory_usage: bool = False, 
+            handle_timeout_termination: bool = True,
+            counter_interval = 100000):
+        super().__init__(buffer_size, observation_space, action_space, device, n_envs, optimize_memory_usage, handle_timeout_termination)
+
+        self.recent_sample_indices = []
+        self.not_recent_sample_indices = []
+        self.counter_interval = counter_interval
+        self.recent_sample_ratio = 0.5
+
+    
+    def add(
+        self,
+        obs: np.ndarray,
+        next_obs: np.ndarray,
+        action: np.ndarray,
+        reward: np.ndarray,
+        done: np.ndarray,
+        infos: List[Dict[str, Any]],
+    ) -> None:
+
+        if len(self.recent_sample_indices) >= self.counter_interval:
+            tmp = self.recent_sample_indices.pop(0)
+            self.not_recent_sample_indices.append(tmp)
+        if len(self.not_recent_sample_indices) >= self.buffer_size - self.counter_interval:
+            self.not_recent_sample_indices.pop(0)
+        self.recent_sample_indices.append(self.pos)
+
+        # if self.recent_sample_ratio < 0.5:
+        #    self.recent_sample_ratio += (0.5 - 0.0) / 2000000
+
+        super().add(obs, next_obs, action, reward, done, infos)
+
+    
+    def sample(self, batch_size: int, env: Optional[VecNormalize] = None) -> ReplayBufferSamples:
+        if not self.optimize_memory_usage:
+            # return super().sample(batch_size=batch_size, env=env)
+            
+            if len(self.recent_sample_indices) < self.counter_interval:
+                return super().sample(batch_size = batch_size, env = env)
+            else:
+                recent_bs = int(batch_size * self.recent_sample_ratio)
+                non_recent_bs = batch_size - recent_bs
+                recent_inds = random.choices(self.recent_sample_indices, k = recent_bs)
+                    
+                # upper_bound = self.buffer_size if self.full else self.pos
+                not_recent_inds = random.choices(self.not_recent_sample_indices, k = non_recent_bs)
+                
+                batch_inds = np.append(recent_inds, not_recent_inds)
+                return self._get_samples(batch_inds, env=env)
+
+
+        if self.full:
+            batch_inds = (np.random.randint(1, self.buffer_size, size=batch_size) + self.pos) % self.buffer_size
+        else:
+            batch_inds = np.random.randint(0, self.pos, size=batch_size)
+
+        return self._get_samples(batch_inds, env=env)
--- train.py
+++ train.py
@@ -4,17 +4,18 @@ import json
 import os
 from stable_baselines3 import DDPG
 from stable_baselines3.her import HerReplayBuffer
+from MyReplayBuffer import MyReplayBuffer
 
 env_name = 'HumanoidPyBulletEnv-v0'
-lr = 0.00002
-total_timesteps = 2000
-old_log_path = './18M/'
-log_path = './3.21/20M/'
+lr = 0.0000002
+total_timesteps = 10000000
+old_log_path = './3.21/23M/'
+log_path = './3.21/33M/'
 load_old_replay_buffer = False
 save_replay_buffer = False
 eval_freq = 1
-batch_size = 256
-replay_buffer_class = None # HerReplayBuffer
+batch_size = 100
+replay_buffer_class = MyReplayBuffer
 tb_log = os.path.join(log_path, 'tb_log')
 eval_log_path = os.path.join(log_path, 'eval')
 
@@ -23,7 +24,7 @@ if not os.path.exists(log_path):
 
 env = gym.make(env_name)
 # model = DDPG('MlpPolicy', env, verbose = 1, learning_rate = lr, tensorboard_log = tb_log)
-model = DDPG.load(os.path.join(old_log_path, 'DDPG'), env, verbose = 1, learning_rate = lr, tensorboard_log = tb_log, batch_size = batch_size, replay_buffer_class = HerReplayBuffer)
+model = DDPG.load(os.path.join(old_log_path, 'DDPG'), env, verbose = 1, learning_rate = lr, tensorboard_log = tb_log, batch_size = batch_size, replay_buffer_class = replay_buffer_class)
 if load_old_replay_buffer:
     model.load_replay_buffer(os.path.join(old_log_path, 'rb'))
 model.learn(total_timesteps, eval_freq = eval_freq, eval_log_path = eval_log_path)
